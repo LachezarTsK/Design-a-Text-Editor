35093b142a1bba1f934d665ab2a63d034e967cb4
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-09-10 12:54:42 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,57 @@
+
+var TextEditor = function () {
+    this.textLeftToCursor = [];
+    this.textRightToCursor = [];
+};
+
+/** 
+ * @param {string} text
+ * @return {void}
+ */
+TextEditor.prototype.addText = function (text) {
+    for (let character of text) {
+        this.textLeftToCursor.push(character);
+    }
+};
+
+/** 
+ * @param {number} numberOfCharsToDelete
+ * @return {number}
+ */
+TextEditor.prototype.deleteText = function (numberOfCharsToDelete) {
+    numberOfCharsToDelete = Math.min(numberOfCharsToDelete, this.textLeftToCursor.length);
+    this.textLeftToCursor.splice(this.textLeftToCursor.length - numberOfCharsToDelete);
+    return numberOfCharsToDelete;
+};
+
+/** 
+ * @param {number} numberOfCharsToMoveToLeft
+ * @return {string}
+ */
+TextEditor.prototype.cursorLeft = function (numberOfCharsToMoveToLeft) {
+    numberOfCharsToMoveToLeft = Math.min(numberOfCharsToMoveToLeft, this.textLeftToCursor.length);
+    while (numberOfCharsToMoveToLeft-- > 0) {
+        this.textRightToCursor.push(this.textLeftToCursor.pop());
+    }
+    return  composeTextToDisplay();
+};
+
+/** 
+ * @param {number} numberOfCharsToMoveToRight
+ * @return {string}
+ */
+TextEditor.prototype.cursorRight = function (numberOfCharsToMoveToRight) {
+    numberOfCharsToMoveToRight = Math.min(numberOfCharsToMoveToRight, this.textRightToCursor.length);
+    while (numberOfCharsToMoveToRight-- > 0) {
+        this.textLeftToCursor.push(this.textRightToCursor.pop());
+    }
+    return composeTextToDisplay();
+};
+
+/** 
+ * @return {string}
+ */
+TextEditor.prototype.composeTextToDisplay = function () {
+    const numberOfCharsToDisplay = Math.min(10, this.textLeftToCursor.length);
+    return this.textLeftToCursor.slice(-numberOfCharsToDisplay).join('');
+};
